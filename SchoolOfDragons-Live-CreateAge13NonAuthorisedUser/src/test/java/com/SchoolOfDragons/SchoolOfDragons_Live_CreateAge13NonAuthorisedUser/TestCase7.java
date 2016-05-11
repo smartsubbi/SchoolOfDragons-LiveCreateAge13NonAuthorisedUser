6a2e3189c1100609258c8b1b296b9f17b161e799
Subbu <subbu.baliga@live.in>
2016-05-11 15:44:06 +0530

    1.0.10 updates

--- SchoolOfDragons-Live-CreateAge13NonAuthorisedUser/src/test/java/com/SchoolOfDragons/SchoolOfDragons_Live_CreateAge13NonAuthorisedUser/TestCase7.java
+++ SchoolOfDragons-Live-CreateAge13NonAuthorisedUser/src/test/java/com/SchoolOfDragons/SchoolOfDragons_Live_CreateAge13NonAuthorisedUser/TestCase7.java
@@ -32,7 +32,7 @@ public class TestCase7
 	ExtentTest logger;	
 	String age = "13";
 	int count = 0;
-	
+	String pathToExcel = "file://172.20.11.105/Usernames%20Excels/";
 	String subject = "Create Age 13 Player (Non Authorized User)";
 	
 	@BeforeClass
@@ -49,55 +49,54 @@ public class TestCase7
 		
 		driver = BrowserFactory.getBrowser("chrome");
 		logger.log(LogStatus.INFO, "Browser is up and running");
-		String browserOpenedScreenshot=logger.addScreenCapture(CaptureScreenshot.takeScreenshot(driver, "Application"));
+		String browserOpenedScreenshot=logger.addScreenCapture(CaptureScreenshot.takeScreenshot(driver, "browserOpenedScreenshot"));
 		logger.log(LogStatus.INFO, browserOpenedScreenshot);		
 		driver.get("http://www.schoolofdragons.com");		
 		logger.log(LogStatus.INFO, "Entered Authentication credentials successfully and Url is Loading");			
 		CommonHeader header = PageFactory.initElements(driver, CommonHeader.class);		
-		logger.log(LogStatus.INFO, "Home Page Title is verified");
-		String homePageScreenshot=logger.addScreenCapture(CaptureScreenshot.takeScreenshot(driver, "Application"));
+		
+		String homePageScreenshot=logger.addScreenCapture(CaptureScreenshot.takeScreenshot(driver, "homePageScreenshot"));
 		logger.log(LogStatus.INFO, homePageScreenshot);				
 		header.clickHeaderCreateAnAccountLink();
 		logger.log(LogStatus.INFO, "Clicked the Create an Account Link on the Homepage header");		
 		Thread.sleep(5000);		
 		SignUpPage signUpPage = PageFactory.initElements(driver, SignUpPage.class);		
-		logger.log(LogStatus.INFO, "Sign Up Page Title is verified");		
-		String signUpPageScreenshot=logger.addScreenCapture(CaptureScreenshot.takeScreenshot(driver, "Application"));
+				
+		String signUpPageScreenshot=logger.addScreenCapture(CaptureScreenshot.takeScreenshot(driver, "signUpPageScreenshot"));
 		logger.log(LogStatus.INFO, signUpPageScreenshot);		
 		signUpPage.confirmButtonDisabledElementValidation();
 		logger.log(LogStatus.INFO, "Verified if the Confirm Button is disabled");		
 		signUpPage.selectAge(age);
 		logger.log(LogStatus.INFO, "Select age 13");		
-		signUpPage.selectedAgeElementValidation(age);
-		logger.log(LogStatus.INFO, "Verify is age selected is 13");		
+		signUpPage.selectedAgeElementValidation(age);			
 		signUpPage.confirmButtonEnabledElementValidation();
-		logger.log(LogStatus.INFO, "Verify if the confirm button is enabled");		
-		String signUpPageAfterAgeSelection=logger.addScreenCapture(CaptureScreenshot.takeScreenshot(driver, "Application"));
+		logger.log(LogStatus.INFO, "Verify if age selected is 13 and confirm button is enabled");		
+		String signUpPageAfterAgeSelection=logger.addScreenCapture(CaptureScreenshot.takeScreenshot(driver, "signUpPageAfterAgeSelection"));
 		logger.log(LogStatus.INFO, signUpPageAfterAgeSelection);		
 		signUpPage.clickConfirmButton();
 		logger.log(LogStatus.INFO, "Click on the Confirm Button");		
 		Thread.sleep(5000);		
 		CreateAnAccountPage createAnAccountPage = PageFactory.initElements(driver, CreateAnAccountPage.class);		
-		logger.log(LogStatus.INFO, "Create an account Page Title is verified");
-		String createAnAccountPageScreenshot=logger.addScreenCapture(CaptureScreenshot.takeScreenshot(driver, "Application"));
+		
+		String createAnAccountPageScreenshot=logger.addScreenCapture(CaptureScreenshot.takeScreenshot(driver, "createAnAccountPageScreenshot"));
 		logger.log(LogStatus.INFO, createAnAccountPageScreenshot);		
 		createAnAccountPage.selectedAgeElementValidation(age);
-		logger.log(LogStatus.INFO, "Verify is age selected is 12");		
+		logger.log(LogStatus.INFO, "Verify if age selected is 12");		
 		String userName = RandomStringGenerator.generateRandomString();
 		String emailAddress = GetNewEmail.getNewEmail(userName);		
 		createAnAccountPage.enterEmail(emailAddress);
-		logger.log(LogStatus.INFO, "Enter Email address");		
+		logger.log(LogStatus.INFO, "Enter Email address : "+emailAddress);		
 		createAnAccountPage.enterUserName(userName);
-		logger.log(LogStatus.INFO, "Enter Username");		
+		logger.log(LogStatus.INFO, "Enter Username : "+userName);		
 		createAnAccountPage.enterPassword("123456");
-		logger.log(LogStatus.INFO, "Enter Password");		
-		String createAnAccountPageAfterEnteringAllDetailsScreenshot=logger.addScreenCapture(CaptureScreenshot.takeScreenshot(driver, "Application"));
+		logger.log(LogStatus.INFO, "Enter Password : 123456");		
+		String createAnAccountPageAfterEnteringAllDetailsScreenshot=logger.addScreenCapture(CaptureScreenshot.takeScreenshot(driver, "createAnAccountPageAfterEnteringAllDetailsScreenshot"));
 		logger.log(LogStatus.INFO, createAnAccountPageAfterEnteringAllDetailsScreenshot);		
 		createAnAccountPage.clickFinishAndPlayButton();		
 		Thread.sleep(5000);		
 		AlmostDonePopUp almostDonePopUp = PageFactory.initElements(driver, AlmostDonePopUp.class);
 		almostDonePopUp.verifyAlmostDonePopUpTextsforPlayer();
-		String almostDonePeopUpScreenshot=logger.addScreenCapture(CaptureScreenshot.takeScreenshot(driver, "Application"));
+		String almostDonePeopUpScreenshot=logger.addScreenCapture(CaptureScreenshot.takeScreenshot(driver, "almostDonePeopUpScreenshot"));
 		logger.log(LogStatus.INFO, almostDonePeopUpScreenshot);		
 		almostDonePopUp.clickAlmostDonePopUpPlayNowButton();		
 		Thread.sleep(5000);		
@@ -105,7 +104,7 @@ public class TestCase7
 		afterLoggedInPage.currentlyLoggedInText(userName).isDisplayed();
 		afterLoggedInPage.afterLoggedInSuccessfully();
 		logger.log(LogStatus.INFO, "After Logged in Page is verified successfully");
-		String afterLoggedinPageScreenshot=logger.addScreenCapture(CaptureScreenshot.takeScreenshot(driver, "Application"));
+		String afterLoggedinPageScreenshot=logger.addScreenCapture(CaptureScreenshot.takeScreenshot(driver, "afterLoggedinPageScreenshot"));
 		logger.log(LogStatus.INFO, afterLoggedinPageScreenshot);		
 		count = count +1;		
 		ExcelDataProvider excel = new ExcelDataProvider();	       
@@ -113,28 +112,62 @@ public class TestCase7
 		logger.log(LogStatus.INFO, "Quitting the Browser Opened");			
 		if(count==1)
 		{
-			WebDriver driver = BrowserFactory.getBrowser("chrome");		
+			//WebDriver driver = BrowserFactory.getBrowser("chrome");		
 			String emailReportPathToSend = ExtentManager.finalPath;
-			String mailContent = "Non authorised user has been created.\n\nYou can refer to the below report for the run result\n"+emailReportPathToSend+"\nBelow are the details of the non authorised user created : \n";
+			//String mailContent = "Non authorised user has been created.\n\nYou can refer to the below report for the run result\n"+emailReportPathToSend+"\nBelow are the details of the non authorised user created : \n";
 			excel.writeToNextFreeCell(2,0,userName);		
 			excel.writetoexcel();
-			SendMail.sendMail(driver,subject,mailContent,age,userName,"123456",emailAddress,"No");
+			//SendMail.sendMail(driver,subject,mailContent,age,userName,"123456",emailAddress,"No");
+			System.out.println("=====================================================================");
+			System.out.println("Created Age 12 Player (Not Authorised User) is : "+userName);
+			System.out.println("Created Age 12 Player (Not Authorised User) password is : 123456");
+			System.out.println("Created Age 12 Player (Not Authorised User) email id is : "+emailAddress);
+			System.out.println("The report can be found here : "+emailReportPathToSend);
+			System.out.println("Path to the Excel file : "+pathToExcel);
+			System.out.println("=====================================================================");
+			logger.log(LogStatus.INFO,"=====================================================================");
+			logger.log(LogStatus.INFO,"Created Age 12 Player (Not Authorised User) is : "+userName);
+			logger.log(LogStatus.INFO,"Created Age 12 Player (Not Authorised User) password is : 123456");
+			logger.log(LogStatus.INFO,"Created Age 12 Player (Not Authorised User) email id is : "+emailAddress);
+			logger.log(LogStatus.INFO,"The report can be found here : "+emailReportPathToSend);
+			logger.log(LogStatus.INFO,"Path to the Excel file : "+pathToExcel);
+			logger.log(LogStatus.INFO,"=====================================================================");
 		}
 		else if(count==2)
 		{
-			WebDriver driver = BrowserFactory.getBrowser("chrome");	
+			//WebDriver driver = BrowserFactory.getBrowser("chrome");	
 			String emailReportPathToSend = ExtentManager.finalPath;
-			String mailContent = "Authorised user has been created.\n\nYou can refer to the below report for the run result\n"+emailReportPathToSend+"\nBelow are the details of the Authorised user created : \n";
+			//String mailContent = "Authorised user has been created.\n\nYou can refer to the below report for the run result\n"+emailReportPathToSend+"\nBelow are the details of the Authorised user created : \n";
 			excel.writeToNextFreeCell(3,0,userName);		
 			excel.writetoexcel();
-			SendMail.sendMail(driver,subject,mailContent,age,userName,"123456",emailAddress,"Yes");			
+			//SendMail.sendMail(driver,subject,mailContent,age,userName,"123456",emailAddress,"Yes");	
+			System.out.println("=====================================================================");
+			System.out.println("Created Age 12 Player (Authorized User) is : "+userName);
+			System.out.println("Created Age 12 Player (Authorized User) password is : 123456");
+			System.out.println("Created Age 12 Player (Authorized User) email id is : "+emailAddress);	
+			System.out.println("The report can be found here : "+emailReportPathToSend);
+			System.out.println("Path to the Excel file : "+pathToExcel);
+			System.out.println("=====================================================================");
+			logger.log(LogStatus.INFO,"=====================================================================");
+			logger.log(LogStatus.INFO,"Created Age 12 Player (Authorized User) is : "+userName);
+			logger.log(LogStatus.INFO,"Created Age 12 Player (Authorized User) password is : 123456");
+			logger.log(LogStatus.INFO,"Created Age 12 Player (Authorized User) email id is : "+emailAddress);	
+			logger.log(LogStatus.INFO,"Path to the Excel file : "+pathToExcel);
+			logger.log(LogStatus.INFO,"=====================================================================");		
+			
 		}
 		else if (count==0)
 		{
-			WebDriver driver = BrowserFactory.getBrowser("chrome");		
+			//WebDriver driver = BrowserFactory.getBrowser("chrome");		
 			String emailReportPathToSend = ExtentManager.finalPath;
-			String mailContent = "User cannot be created as there are issues You can refer to the below report for the run result\n"+emailReportPathToSend;
-			SendMail.sendMail(driver,subject,mailContent,"Not Created","Not Created","Not Created","Not Created","Not Created");
+			//String mailContent = "User cannot be created as there are issues You can refer to the below report for the run result\n"+emailReportPathToSend;
+			//SendMail.sendMail(driver,subject,mailContent,"Not Created","Not Created","Not Created","Not Created","Not Created");
+			System.out.println("=====================================================================");
+			System.out.println("User cannot be created as there are issues You can refer to the below report for the run result\n"+emailReportPathToSend);				
+			System.out.println("=====================================================================");	
+			logger.log(LogStatus.INFO,"=====================================================================");
+			logger.log(LogStatus.INFO,"User cannot be created as there are issues You can refer to the below report for the run result\n"+emailReportPathToSend);				
+			logger.log(LogStatus.INFO,"=====================================================================");	
 			
 		}
 		BrowserFactory.closeBrowser();
